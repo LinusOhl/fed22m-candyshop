38773d1085bfa95a12c59918790f92bda7412bd1
Linuss <tade3d@gmail.com>
2022-12-21 11:55:50 +0100

    Renders all candy from api to DOM as cards.

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,16 @@ const data: ICandy[] = temp.data;
 console.log(data);
 
 const renderAllCandy = () => {
+  const base_url = "https://www.bortakvall.se";
+
   candyListEl.innerHTML = data.map(candy => `
-    <li>${candy.name}</li>
+    <li class="candy-card">
+      <img src="${base_url + candy.images.thumbnail}" alt="${candy.name}">
+      <p class="candy-name" id="candy-name">${candy.name}</p>
+      <p class="candy-price" id="candy-price">${candy.price}kr</p>
+      <button id="btn-addToCart">Add to cart</button>
+    </li>
   `)
   .join("");
 };
-// renderAllCandy();
+renderAllCandy();
