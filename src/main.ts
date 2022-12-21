8315f66a040dc0759176af1d3ac76fcc4764ecf9
Linuss <tade3d@gmail.com>
2022-12-21 11:06:39 +0100

    All candy is display in a list.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import './style.css'
 import { fetchAllCandy } from "./api"
 import { ICandy } from "./interfaces"
 
+const candyListEl = document.querySelector("#candy-list")!;
+
 const temp = await fetchAllCandy();
 const data: ICandy[] = temp.data;
 console.log(data);
+
+const renderAllCandy = () => {
+  candyListEl.innerHTML = data.map(candy => `
+    <li>${candy.name}</li>
+  `)
+  .join("");
+};
+renderAllCandy();
