978f17b65e3c57933a2691a79986cad17eedfafa
Philippe Le Gall <philippelegall@live.se>
2022-12-23 00:39:01 +0100

    modify text and added typescript for cart.

---
+++ src/cart.ts
@@ -0,0 +1,33 @@
+    import { ICandy } from "./interfaces"
+
+    
+
+   let cartItems: ICandy[] = [];
+  
+
+    const addCandy = (item: ICandy) => {
+      cartItems.push(item);
+    }
+
+    const removeCandy = (item: ICandy) => {
+      const index = cartItems.indexOf(item);
+      if (index >= 0) {
+        cartItems.splice(index, 1);
+      }
+    }
+
+    const getTotal = () => {
+      let total = 0;
+      for (const item of cartItems) {
+        total += item.price;
+      }
+      return total;
+    }
+
+    const show = () => {
+
+    }
+
+    const hide = () => {
+
+    }
