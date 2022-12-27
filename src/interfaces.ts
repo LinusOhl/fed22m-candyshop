9d26b22103c982d9f92e08cb2461bdd62c55887c
Linuss <tade3d@gmail.com>
2022-12-27 13:12:42 +0100

    Created interface for orders.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,3 +16,21 @@ export interface ICandy {
   stock_status: string;
   stock_quantity: null;
 }
+
+export interface IOrder {
+  customer_first_name: string;
+  customer_last_name: string;
+  customer_address: string;
+  customer_postcode: string;
+  customer_city: string;
+  customer_email: string;
+  order_total: number;
+  order_items: [
+    {
+      product_id: number;
+      qty: number;
+      item_price: number;
+      item_total: number;
+    }
+  ]
+}
